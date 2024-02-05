b3b8d70a6624d427c4d26fe4dc9abf4dd19187ea
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-02-05 23:22:06 +0300

    Initial Commit

---
+++ Timer/TimerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TimerApp.swift
+//  Timer
+//
+//  Created by Wijdan Almutairi on 05/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct TimerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
