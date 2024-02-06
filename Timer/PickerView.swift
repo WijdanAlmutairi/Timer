84cb08bcdd2bbba71047b0b3de77d9fa6c5b1a4b
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-02-06 23:13:29 +0300

    feat(picker-view): create the picker view component
    - Implemented the picker view swift ui component
    - Added functionality to handle user selection

---
+++ Timer/PickerView.swift
@@ -0,0 +1,86 @@
+//
+//  PickerView.swift
+//  Timer
+//
+//  Created by Wijdan Almutairi on 06/02/2024.
+//
+
+import SwiftUI
+
+struct PickerView: View {
+    let hours = Array(0...23)
+    let minutes = Array(0...59)
+    let seconds = Array(0...59)
+    
+    @State private var selectedHour: Int = 0
+    @State private var selectedMinute: Int = 0
+    @State private var selectedSecond: Int = 0
+    @State private var text: String = "" 
+    var body: some View {
+        NavigationView {
+            VStack {
+                Form {
+                    Section(header: Text("")) {
+                        HStack {
+                            //picker one
+                            Picker(selection: $selectedHour, label: Text("hours")) {
+                                    ForEach (hours, id: \.self) { hour in
+                                        Text("\(hour)")
+                                    }
+                                    
+                            }.pickerStyle(.wheel)
+                            Text("hours")
+                                .bold()
+                            
+                            //picker two
+                            Picker(selection: $selectedMinute, label: Text("hours")) {
+                                    ForEach (minutes, id: \.self) { minute in
+                                        Text("\(minute)")
+                                    }
+                                    
+                            }.pickerStyle(.wheel)
+                            Text("min")
+                                .bold()
+                            
+                            //picker three
+                            Picker(selection: $selectedSecond, label: Text("hours")) {
+                                    ForEach (seconds, id: \.self) { second in
+                                        Text("\(second)")
+                                    }
+                                    
+                            }.pickerStyle(.wheel)
+                            Text("sec")
+                                .bold()
+                        }
+                    }
+                    Section(header: Text("")){
+                        TextField("Label", text: $text)
+                    }
+                    
+                }
+                
+                
+                Button(action: {
+                            // Add your action here
+                            print("h \(selectedHour) ")
+                            print("m \(selectedMinute)")
+                            print("s \(selectedSecond)")
+                            print("label \(text)")
+                    
+                        }) {
+                            Text("Start")
+                                .padding(100)
+                                .background(Color.clear)
+                                .font(.largeTitle)
+                                .bold()
+                        }
+            }
+           
+        .navigationTitle("Timer")
+        }
+    }
+}
+
+#Preview {
+    PickerView()
+}
