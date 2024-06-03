6f0bf481b00f8e9ab11c766c1fb8d48fc01fe1d5
Wijdan Almutairi <wijdan.s.almutairi@gmail.com>
2024-06-03 23:57:27 +0300

    feat(timer): add countdown logic for hours, minutes, and seconds
    - Implemented logic in TimerView to count down from hours, minutes, and seconds
    - Displayed formatted time string (HH:mm:ss) on the screen

--- Timer/TimerView.swift
+++ Timer/TimerView.swift
@@ -12,21 +12,29 @@ struct TimerView: View {
     @Binding var selectedMinute: Int
     @Binding var selectedSecond: Int
     @Binding var label: String
-    
-    // @State var countDownTimer = 5
     @Binding var isTimerRunning: Bool
+    
+    @State var timeRemaining = 0
     let timer = Timer.publish(every: 1, on: .main, in: .common).autoconnect()
+    
     var body: some View {
         VStack {
-            Text("\(selectedSecond)")
-                .onReceive(timer) { _ in
-                    if(selectedSecond > 0 && isTimerRunning){
-                        selectedSecond -= 1
-                    } else {
-                        isTimerRunning = false
-                    }
-                }.font(.system(size: 80, weight: .bold))
-                .opacity(0.8)
+            HStack{
+                Text("\(convertSecondsToTime(timeInSeconds: timeRemaining))")
+                    .onReceive(timer) { _ in
+                        // calculate time
+                            timeRemaining = calculateTimeInSeconds(hours: selectedHour, minutes: selectedMinute, seconds: selectedSecond)
+                        
+                        if(timeRemaining > 0 && isTimerRunning){
+                            timeRemaining -= 1
+                        }
+                        
+                        else {
+                            isTimerRunning = false
+                        }
+                    }.font(.system(size: 80, weight: .bold))
+                    .opacity(0.8)
+            }
         }
         
         HStack{
@@ -38,6 +46,31 @@ struct TimerView: View {
                 //countDownTimer = 5
             }
         }
+    }
+    
+    func convertSecondsToTime(timeInSeconds: Int) -> String{
+        var hours = 0
+        var minutes = 0
+        var seconds = 0
+    
+            hours = timeInSeconds / 3600
+            minutes = (timeInSeconds % 3600) / 60
+            seconds = (timeInSeconds % 3600) % 60
+            
+        return String(format: "%02i:%02i:%02i", hours, minutes, seconds)
+    }
+    
+    func calculateTimeInSeconds(hours: Int, minutes: Int, seconds: Int) -> Int {
+        var totalSeconds = 0
+        
+        if(hours > 0 || minutes > 0 || seconds > 0){
+            let hourTotal = hours * 3600
+            let minuteTotal = minutes * 60
+            totalSeconds = hourTotal + minuteTotal +  seconds
+            print(totalSeconds)
+        }
+        
+        return totalSeconds
     }
 }
 
